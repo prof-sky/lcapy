e565e9f113f622c3bc46a092f177004d74336021
Yannick Wieland <yannick.wieland@gmail.com>
2025-03-28 08:46:03 +0100

    an object to bundle the data created from the simplifying stepwise function
    component relation defines wich relation two components can have for code readability

---
+++ lcapy/solutionStep.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from lcapy.circuit import Circuit
+
+from lcapy.componentRelation import ComponentRelation
+from typing import Union
+
+
+class SolutionStep:
+    def __init__(self, circuit: Circuit, cpts: list[str], newCptName: str,
+                 relation: ComponentRelation, lastStep: Circuit, nextStep: Circuit
+                 ):
+        self.circuit: Circuit = circuit
+        self.cpts: list[str] = cpts
+        self.newCptName: Union[str, None] = newCptName
+        self.relation: ComponentRelation = relation
+        self.isInitialStep: bool = not (self.cpts or self.newCptName or self.relation)
+        self.lastStep: Union[Circuit, None] = lastStep
+        self.nextStep: Union[Circuit, None] = nextStep
+
+    def draw(self):
+        self.circuit.draw()
