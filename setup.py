2cb59e4445305b9162bab8c154e47b02ca84c25f
Yannick Wieland <yannick.wieland@hs-pforzheim.de>
2025-12-08 10:44:40 +0100

    rewrite build script in python

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
-__version__ = '0.38.8'
+__version__ = '0.38.13'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
