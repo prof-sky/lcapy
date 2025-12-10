631fd1480bdd29b94408f1eeb12ae9944804b45c
Yannick Wieland <yannick.wieland@hs-pforzheim.de>
2025-12-10 17:08:04 +0100

    test new hashing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
-__version__ = '0.38.13'
+__version__ = '0.39.0'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
